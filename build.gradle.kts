f684b5da1769632e095976946bd803f2c86145c5
aesperer <piyrw9754@gmail.com>
2023-08-19 17:27:33 +0900

    create launcher

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,15 @@ allprojects {
     }
 
     dependencies {
-        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.1")
+      implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.1")
+      implementation(project(":proto-lib"))
+      implementation("com.linecorp.armeria:armeria:1.9.2")
+      implementation("com.linecorp.armeria:armeria-grpc:1.9.2")
+      implementation("io.github.microutils:kotlin-logging:1.12.5")
+      implementation("ch.qos.logback:logback-classic:1.2.3")
+
+      testImplementation("io.kotest:kotest-assertions-core:4.6.1")
+      testImplementation("io.kotest:kotest-extensions-spring:4.4.3")
+      testImplementation("io.kotest:kotest-runner-junit5:4.6.1")
     }
 }
